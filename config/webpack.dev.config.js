b67d12048dd55326b6800eec96d78fc1e17946ec
xiaoyu-m <Xiaoyu0625>
2020-07-18 13:15:05 +0800

    router

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -6,5 +6,5 @@ module.exports = merge(baseConfig, {
   mode: "development",
   devtool: "inline-source-map",
   // 配置服务端目录和端口
-  devServer: { disableHostCheck: true, contentBase: "../dist", port: 80 },
+  devServer: { disableHostCheck: true, contentBase: "../dist", port: 3000 },
 });
